78915fb9f7420300faf7003fea6013e624110565
Baptiste Barreyre <baptistebarreyre@hotmail.fr>
2022-01-04 21:27:12 +0100

    Background + FishArrival work

--- JuicyFish/Assets/FishMove.cs
+++ JuicyFish/Assets/FishMove.cs
@@ -1,29 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using DG.Tweening;
+using UnityEngine;
 
 public class FishMove : MonoBehaviour
 {
     public Vector3 positionToGo;
     [SerializeField]
     private float speed;
-
+    [SerializeField]
+    private float socialDistancing;
+    private float security = 1;
     private float sinusTime;
+
+
     private void Start()
     {
-        positionToGo = (Random.insideUnitCircle * 2) * positionToGo;
-
-
+        RandomPosition();
     }
 
+    private void RandomPosition()
+    {
+        positionToGo = (Random.insideUnitCircle * security) * positionToGo;
+        positionToGo = new Vector3(positionToGo.x, positionToGo.y + security);
+
+        foreach (FishMove Fish in FindObjectsOfType<FishMove>())
+        {
+            if (Fish != this)
+            {
+                if (Vector3.Distance(positionToGo, Fish.positionToGo) < socialDistancing)
+                {
+                    security++;
+                    RandomPosition();
+                    return;
+                }
+
+            }
+        }
+    }
 
     void Update()
-    { 
+    {
         transform.DOMoveX(positionToGo.x, speed);
 
         sinusTime += Time.deltaTime;
-        if(sinusTime > speed)
+        if (sinusTime > speed)
         {
             return;
         }

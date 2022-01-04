afbd2c38fcd285266519ce24070f97d3c24e92dd
SpaceCowboy2 <julien.dor@edu.devinci.fr>
2022-01-04 13:07:57 +0100

    SpaceInvader minigame

---
+++ JuicyFish/Assets/Julien/Scripts/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class GameOver : MonoBehaviour {
+
+	public static bool isPlayerDead = false;
+	private TextMeshProUGUI gameOver;
+
+	void Start () {
+		gameOver = GetComponent<TextMeshProUGUI> ();
+		gameOver.enabled = false;
+	}
+
+	void Update () {
+		if (isPlayerDead) {
+			Time.timeScale = 0;
+			gameOver.enabled = true;
+		}
+	}
+}
